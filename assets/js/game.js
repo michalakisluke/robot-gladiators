6a2ced662e50543428fba5a67c8c7ac0d83da220
Luke Michalakis <michalakisluke@gmail.com>
2021-08-16 12:24:11 -0400

    <Added more javascript commands>

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?")
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+console.log(playerName + " is ready for battle!");
+console.log("Your robot, " + playerName + ", has won!");
+function fight() {
+    window.alert("The fight has begun!")
+}
+//fight();
